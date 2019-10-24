471b0f6d87d4d76eeed9ac35e8a85bef6c3869e9
eastline <eastline@maycan.co.kr>
2019-10-24 18:09:24 +0900

    #6.1 Room Admin Panel part Two

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,13 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = ["django_countires"]
 
 PROJECT_APPS = [
-    "core.apps.CoreConfig", "users.apps.UsersConfig", "rooms.apps.RoomsConfig"
+    "core.apps.CoreConfig", 
+    "users.apps.UsersConfig", 
+    "rooms.apps.RoomsConfig", 
+    "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
+    "conversations.apps.ConversationsConfig"
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
--- rooms/admin.py
+++ rooms/admin.py
@@ -2,7 +2,65 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    """ Room Admin Definition """
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("More About the Space", {
+            "classes": ("collapse",),
+            "fields": ("amenities", "facilities", "house_rules")}),
+        ("Last Details", {"fields": ("host",)})
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ """
 
     pass
