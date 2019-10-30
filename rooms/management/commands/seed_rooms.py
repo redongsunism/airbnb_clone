2a776fab3ed71e43580dcc5674ebbb0e5398495e
eastline <eastline@maycan.co.kr>
2019-10-31 01:17:37 +0900

    seed_rooms part Three

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,5 +1,6 @@
 import random
 from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten
 from django_seed import Seed
 from rooms import models as room_models
 from users import models as user_models
@@ -20,17 +21,28 @@ class Command(BaseCommand):
         seeder = Seed.seeder()
         all_users = user_models.User.objects.all()
         room_types = room_models.RoomType.objects.all()
-        seeder.add_entity(room_models.Room, number,
-        {
-            "name": lambda x: seeder.faker.address(),
-            "host": lambda x: random.choice(all_users),
-            "room_type": lambda x: random.choice(room_types),
-            "guests": lambda x: random.randint(1, 20),
-            "price": lambda x: random.randint(1, 300),
-            "beds": lambda x: random.randint(1, 5),
-            "bedrooms": lambda x: random.randint(1, 5),
-            "baths": lambda x: random.randint(1, 5),
-
-        })
-        seeder.execute()
+        seeder.add_entity(
+            room_models.Room,
+            number,
+            {
+                "name": lambda x: seeder.faker.address(),
+                "host": lambda x: random.choice(all_users),
+                "room_type": lambda x: random.choice(room_types),
+                "guests": lambda x: random.randint(1, 20),
+                "price": lambda x: random.randint(1, 300),
+                "beds": lambda x: random.randint(1, 5),
+                "bedrooms": lambda x: random.randint(1, 5),
+                "baths": lambda x: random.randint(1, 5),
+            },
+        )
+        created_photos = seeder.execute()
+        created_clean = flatten(list(created_photos.values()))
+        for pk in created_clean:
+            room = room_models.Room.objects.get(pk=pk)
+            for i in range(3, random.randint(10, 17)):
+                room_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    file=f"/room_photos/{random.randint(1, 31)}.webp",
+                    room=room,
+                )
         self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
