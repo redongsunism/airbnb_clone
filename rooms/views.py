ad261fe6a2d4c9c46aedd35b65528c07035c08b5
eastline <eastline@maycan.co.kr>
2019-11-01 10:47:34 +0900

    #12.3 Http404()

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,23 @@
-from math import ceil
+from django.views.generic import ListView
+from django.http import Http404
 from django.shortcuts import render
-from django.core.paginator import Paginator
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.page(int(page))
-    return render(request, "rooms/home.html", {"page": rooms})
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()
