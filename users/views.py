b143a2b1135a43e1a45764116ea7bbb2d7b2163b
eastline <eastline@maycan.co.kr>
2019-11-22 00:57:18 +0900

    #16.4 Completing Verification

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
-from . import forms
+from . import forms, models
 
 
 class LoginView(FormView):
@@ -32,9 +32,9 @@ class SignUpView(FormView):
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
     initial = {
-        "first_name":"Dongseon",
-        "last_name":"Lee",
-        "email":"eastline@maycan.co.kr",
+        "first_name": "Dongseon",
+        "last_name": "Lee",
+        "email": "eastline@maycan.co.kr",
     }
 
     def form_valid(self, form):
@@ -44,4 +44,18 @@ class SignUpView(FormView):
         user = authenticate(self.request, username=email, password=password)
         if user is not None:
             login(self.request, user)
+        user.verify_email()
         return super().form_valid(form)
+
+
+def complete_verification(request, key):
+    try:
+        user = models.User.objects.get(email_secret=key)
+        user.email_verified = True
+        user.email_secret = ""
+        user.save()
+        # to do: add succes message
+    except models.User.DoesNotExist:
+        # to do: add error message
+        pass
+    return redirect(reverse("core:home"))
