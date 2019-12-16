67dd2b8eb523397014448765c168d3b13d5b19e0
eastline <eastline@maycan.co.kr>
2019-12-17 00:30:21 +0900

    #24.13 Updating a Reservation

---
+++ reservations/managers.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class CustomReservationManager(models.Manager):
+
+    def get_or_none(self, **kwargs):
+        try:
+            return self.get(**kwargs)
+        except self.model.DoseNotExist:
+            return None
+            
