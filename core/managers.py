1ff1fd99131531ef99082c3a222c1584f7007b1c
eastline <eastline@maycan.co.kr>
2019-12-17 16:34:38 +0900

    24.16 Validate Everything

--- core/managers.py
+++ core/managers.py
@@ -1,11 +1,9 @@
 from django.db import models
 
 
-class CustomReservationManager(models.Manager):
-
+class CustomModelManager(models.Manager):
     def get_or_none(self, **kwargs):
         try:
             return self.get(**kwargs)
         except self.model.DoseNotExist:
             return None
-            
