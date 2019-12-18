6590bd789609e81ff6bcc689b9aa440eaa999ed2
eastline <eastline@maycan.co.kr>
2019-12-19 01:13:04 +0900

    25.10 Conclusions!

--- core/managers.py
+++ core/managers.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import UserManager
 
 
 class CustomModelManager(models.Manager):
@@ -7,3 +8,6 @@ class CustomModelManager(models.Manager):
             return self.get(**kwargs)
         except self.model.DoseNotExist:
             return None
+
+class CustomUserManager(CustomModelManager, UserManager):
+    pass
